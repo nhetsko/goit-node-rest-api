c0443d57f96bcf0224b9a617ed59db2edc05dfb2
Nadiia <nadiahecko@gmail.com>
2024-05-07 15:05:32 +0300

    add

--- db.js
+++ db.js
@@ -9,14 +9,3 @@ mongoose
     console.error(error);
     process.exit(1);
   });
-// import { connect } from "mongoose";
-
-// export async function connectDb() {
-//   try {
-//     await connect(process.env.DB_URI);
-//     console.log("Database connection successfully");
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-// }
